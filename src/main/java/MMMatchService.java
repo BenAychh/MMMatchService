2c55aaf4cd674a2806488be5cc5f1cbf56f9d19b
BenAychh <ben.tomas.hernandez@gmail.com>
2016-05-27 00:34:54 -0600

    Added Mongo routes

---
+++ src/main/java/MMMatchService.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.logging.Level;
+import org.json.JSONObject;
+import java.util.logging.Logger;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import static spark.Spark.delete;
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
+import static spark.Spark.put;
+
+/**
+ *
+ * @author benhernandez
+ */
+public class MMMatchService {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) throws Exception {
+    Logger mongoLogger = Logger.getLogger( "org.mongodb" );
+    mongoLogger.setLevel(Level.SEVERE); // e.g. or Log.WARNING, etc.
+    port(8002);
+    MongoRoutes mongoRoutes = new MongoRoutes();
+    post("/upsert", mongoRoutes.createOrUpdate);
+    get("/suggestions", mongoRoutes.getPotentialMatches);
+    put("/deactivate", mongoRoutes.deactivate);
+    put("/activate", mongoRoutes.activate);
+    
+    PostgresRoutes postgresRoutes = new PostgresRoutes();
+    get("/matches", postgresRoutes.getMatches);
+    get("/interested", postgresRoutes.getInterested);
+    put("/addinterest", postgresRoutes.putInterest);
+    put("/removeinterest", postgresRoutes.removeInterest);
+    get("*", error);
+    post("*", error);
+    put("*", error);
+    delete("*", error);
+  }
+  
+  private static Route error = new Route() {
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        JSONObject res = new JSONObject();
+        res.put("message", "error");
+        res.put("status", 404);
+        res.put("requested resource", request.pathInfo());
+        res.put("requested method", request.requestMethod());
+        response.status(404);
+        return res.toString();
+    }
+  };
+}
---
+++ src/main/java/MongoRoutes.java
@@ -0,0 +1,169 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import com.goebl.david.Webb;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+/**
+ *
+ * @author benhernandez
+ */
+public class MongoRoutes {
+  public static final String DATABASE_NAME = "potentialMatches";
+  public static final String TABLE_NAME = "potentialMatches";
+  
+  public final Route getPotentialMatches = new Route() {
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+      MongoClient mc = getMongoClient();
+      MongoDatabase md = mc.getDatabase(DATABASE_NAME);
+      MongoCollection<Document> potentialMatches
+          = md.getCollection(TABLE_NAME);
+      JSONObject returner = new JSONObject();
+      Document matcher = new Document("email", request.queryParams("email"));
+      Document profile = potentialMatches.find(matcher).first();
+      if (profile != null) {
+        returnReady(200, "User found", returner, response);
+        returner.put("match suggestions", profile.get("matchSuggestions"));
+      } else {
+        returnReady(400, request.queryParams("email") + " does not exist",
+            returner, response);
+      }
+      return returner;
+    }
+  };
+  public final Route createOrUpdate = new Route() {
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+      JSONObject data = new JSONObject(request.body());
+      MongoClient mc = getMongoClient();
+      MongoDatabase md = mc.getDatabase(DATABASE_NAME);
+      MongoCollection<Document> potentialMatches
+          = md.getCollection(TABLE_NAME);
+      // Check if it already exists
+      JSONObject returner = new JSONObject();
+      Document matcher = new Document("email", data.getString("email"));
+      if (potentialMatches.find(matcher).first() == null) {
+        potentialMatches.insertOne(convertJSONtoDocument(data));
+        if (notifyDaemon(data.getString("email"), true)) {
+          returnReady(201, data.getString("email") + " added, starting "
+              + "match calculations", returner, response);
+        } else {
+          returnReady(500, "Internal Daemon Error", returner, response);
+        }
+      } else {
+        System.out.println(convertJSONtoDocument(data));
+        potentialMatches.updateOne(matcher,
+            new Document("$set", convertJSONtoDocument(data)));
+        if (notifyDaemon(data.getString("email"), true)) {
+          returnReady(200, data.getString("email") + " updated, starting "
+              + "match calculations", returner, response);
+        } else {
+          returnReady(500, "Internal Daemon Error", returner, response);
+        }
+      }
+      mc.close();
+      return returner;
+    }
+  };
+  public final Route deactivate = new Route() {
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+      JSONObject data = new JSONObject(request.body());
+      MongoClient mc = getMongoClient();
+      MongoDatabase md = mc.getDatabase(DATABASE_NAME);
+      MongoCollection<Document> potentialMatches
+          = md.getCollection(TABLE_NAME);
+      JSONObject returner = new JSONObject();
+      Document matcher = new Document("email", data.getString("email"));
+      Document active = new Document("$set", new Document("active", false));
+      try {
+        Document profile = potentialMatches.findOneAndUpdate(matcher, active);
+        returnReady(200, data.getString("email") + " deactivated",
+            returner, response);
+        notifyDaemon(data.getString("email"), false);
+      } catch (Exception e) {
+        System.out.println("Error: " + e);
+      }
+      mc.close();
+      return returner;
+    }
+  };
+  public final Route activate = new Route() {
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+      JSONObject data = new JSONObject(request.body());
+      MongoClient mc = getMongoClient();
+      MongoDatabase md = mc.getDatabase(DATABASE_NAME);
+      MongoCollection<Document> potentialMatches
+          = md.getCollection(TABLE_NAME);
+      JSONObject returner = new JSONObject();
+      Document matcher = new Document("email", data.getString("email"));
+      Document active = new Document("$set", new Document("active", true));
+      try {
+        Document profile = potentialMatches.findOneAndUpdate(matcher, active);
+        returnReady(200, data.getString("email") + " activated",
+            returner, response);
+        notifyDaemon(data.getString("email"), false);
+      } catch (Exception e) {
+        System.out.println("Error: " + e);
+      }
+      mc.close();
+      return returner;
+    }
+  };
+
+  private final MongoClient getMongoClient() {
+    final String DEFAULT_MONGO_PORT = "27017";
+    String host = System.getenv("MD_PORT_27017_TCP_ADDR");
+    if (host == null) {
+      host = "localhost";
+    }
+    String port = System.getenv("MD_PORT_27017_TCP_PORT");
+    if (port == null) {
+      port = DEFAULT_MONGO_PORT;
+    }
+    return new MongoClient(host, Integer.parseInt(port));
+  }
+  
+  private Document convertJSONtoDocument(final JSONObject obj) {
+    Document returner = new Document();
+    obj.keySet().forEach(key -> {
+      returner.append(key, obj.get(key));
+    });
+    return returner;
+  }
+  
+  private void returnReady(final int status, final String message,
+      final JSONObject obj, final Response response) {
+    obj.put("status", status);
+    obj.put("message", message);
+    response.status(status);
+  }
+  
+  private boolean notifyDaemon(final String email, final boolean updated)
+  {
+//    String daemon = System.getenv("daemonIP");
+//    if (daemon == null) {
+//      daemon = "localhost";
+//    }
+//    Webb webb = Webb.create();
+//    com.goebl.david.Request request = webb.post(daemon + "/notify")
+//        .param("email", email)
+//        .param("update", updated);
+//    com.goebl.david.Response<Void> response = request.asVoid();
+//    return response.getStatusCode() == 200;
+    return true;
+  }
+}
